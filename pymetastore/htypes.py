b7baefc7b70189e4c9f3c186157d895df21e1e45
Chris Riccomini <criccomini@apache.org>
2023-06-21 09:54:44 -0700

    Fix pyright and pylint errors
    I went through and did some tweaks to make pylint pass with >= 7.0 score
    (currently at 7.6). I also tweaked the types to make them work with pyright.

--- pymetastore/htypes.py
+++ pymetastore/htypes.py
@@ -1,7 +1,7 @@
-from typing import List, Any
 from collections import namedtuple
 from enum import Enum
-import pprint
+from typing import Any, List, Optional
+
 
 class PrimitiveCategory(Enum):
     VOID = "VOID"
@@ -24,6 +24,7 @@ class PrimitiveCategory(Enum):
     INTERVAL_DAY_TIME = "INTERVAL_DAY_TIME"
     UNKNOWN = "UNKNOWN"
 
+
 class HTypeCategory(Enum):
     PRIMITIVE = PrimitiveCategory
     STRUCT = "STRUCT"
@@ -31,31 +32,37 @@ class HTypeCategory(Enum):
     LIST = "LIST"
     UNION = "UNION"
 
+
 class HType:
     def __init__(self, name: str, category: HTypeCategory):
         self.name = name
         self.category = category
 
+
 class HPrimitiveType(HType):
-    def __init__(self, primitiveType: PrimitiveCategory):
-        super().__init__(primitiveType.value, HTypeCategory.PRIMITIVE)
-        self.primitiveType = primitiveType
+    def __init__(self, primitive_type: PrimitiveCategory):
+        super().__init__(primitive_type.value, HTypeCategory.PRIMITIVE)
+        self.primitive_type = primitive_type
+
 
 class HMapType(HType):
-    def __init__(self, keyType: HType, valueType: HType):
+    def __init__(self, key_type: HType, value_type: HType):
         super().__init__("MAP", HTypeCategory.MAP)
-        self.keyType = keyType
-        self.valueType = valueType
+        self.key_type = key_type
+        self.value_type = value_type
+
 
 class HListType(HType):
-    def __init__(self, elementType: HType):
+    def __init__(self, element_type: HType):
         super().__init__("LIST", HTypeCategory.LIST)
-        self.elementType = elementType
+        self.element_type = element_type
+
 
 class HUnionType(HType):
     def __init__(self, types: List[HType]):
         super().__init__("UNION", HTypeCategory.UNION)
         self.types = types
+
 
 class HVarcharType(HType):
     def __init__(self, length: int):
@@ -65,6 +72,7 @@ class HVarcharType(HType):
         super().__init__("VARCHAR", HTypeCategory.PRIMITIVE)
         self.length = length
 
+
 class HCharType(HType):
     def __init__(self, length: int):
         MAX_CHAR_LENGTH = 255
@@ -72,6 +80,7 @@ class HCharType(HType):
             raise ValueError("Char length cannot exceed 255")
         super().__init__("CHAR", HTypeCategory.PRIMITIVE)
         self.length = length
+
 
 class HDecimalType(HType):
     def __init__(self, precision: int, scale: int):
@@ -85,20 +94,23 @@ class HDecimalType(HType):
         self.precision = precision
         self.scale = scale
 
+
 class HStructType(HType):
-    def __init__(self, names: List[str], types:List[HType]):
+    def __init__(self, names: List[str], types: List[HType]):
         if len(names) != len(types):
             raise ValueError("mismatched size of names and types.")
         if None in names:
             raise ValueError("names cannot contain None")
         if None in types:
             raise ValueError("types cannot contain None")
-        
+
         super().__init__("STRUCT", HTypeCategory.STRUCT)
         self.names = names
         self.types = types
 
-# We need to maintain a list of the expected serialized type names. Thrift will return the type in string format and we will have to parse it to get the type.
+
+# We need to maintain a list of the expected serialized type names.
+# Thrift will return the type in string format and we will have to parse it to get the type.
 class SerdeTypeNameConstants(Enum):
     # Primitive types
     VOID = "void"
@@ -126,7 +138,9 @@ class SerdeTypeNameConstants(Enum):
     UNION = "uniontype"
     UNKNOWN = "unknown"
 
-# We need a mapping between the two definitions of primitive types. The first is the one used by Hive and the second is the one used by Thrift returned values.
+
+# We need a mapping between the two definitions of primitive types.
+# The first is the one used by Hive and the second is the one used by Thrift returned values.
 primitive_to_serde_mapping = {
     PrimitiveCategory.VOID: SerdeTypeNameConstants.VOID,
     PrimitiveCategory.BOOLEAN: SerdeTypeNameConstants.BOOLEAN,
@@ -146,20 +160,22 @@ primitive_to_serde_mapping = {
     PrimitiveCategory.CHAR: SerdeTypeNameConstants.CHAR,
     PrimitiveCategory.INTERVAL_YEAR_MONTH: SerdeTypeNameConstants.INTERVAL_YEAR_MONTH,
     PrimitiveCategory.INTERVAL_DAY_TIME: SerdeTypeNameConstants.INTERVAL_DAY_TIME,
-    PrimitiveCategory.UNKNOWN: SerdeTypeNameConstants.UNKNOWN
+    PrimitiveCategory.UNKNOWN: SerdeTypeNameConstants.UNKNOWN,
 }
 
-# We also need the inverse though. 
+# We also need the inverse though.
 serde_to_primitive_mapping = {v.name: k for k, v in primitive_to_serde_mapping.items()}
+
 
 # This function gets us the serde type name from the primitive one.
 def primitive_to_serde(primitive_category: PrimitiveCategory) -> SerdeTypeNameConstants:
-    primitive_to_serde_mapping.get(primitive_category)
-    return primitive_to_serde_mapping.get(primitive_category)
+    return primitive_to_serde_mapping[primitive_category]
+
 
 # This function gets us the primitive type name from the serde one.
-def serde_to_primitive(serde_type_name: str) -> PrimitiveCategory:
+def serde_to_primitive(serde_type_name: str) -> Optional[PrimitiveCategory]:
     return serde_to_primitive_mapping.get(serde_type_name)
+
 
 # We also need a way to get the serde enum key from the string we get from Thrift.
 def get_serde_type_by_value(value):
@@ -168,8 +184,10 @@ def get_serde_type_by_value(value):
             return member.name
     return None
 
-# To be used for tokening the Thrift type string. We need to tokenize the string to get the type name and the type parameters.
-class Token(namedtuple('Token', ['position', 'text', 'type'])):
+
+# To be used for tokening the Thrift type string. We need to tokenize the
+# string to get the type name and the type parameters.
+class Token(namedtuple("Token", ["position", "text", "type"])):
     def __new__(cls, position: int, text: str, type_: bool) -> Any:
         if text is None:
             raise ValueError("text is null")
@@ -178,7 +196,8 @@ class Token(namedtuple('Token', ['position', 'text', 'type'])):
     def __str__(self):
         return f"{self.position}:{self.text}"
 
-class PrimitiveParts(namedtuple('PrimitiveParts', ['type_name', 'type_params'])):
+
+class PrimitiveParts(namedtuple("PrimitiveParts", ["type_name", "type_params"])):
     def __new__(cls, type_name: str, type_params: List[str]) -> Any:
         if type_name is None:
             raise ValueError("type_name is null")
@@ -189,40 +208,49 @@ class PrimitiveParts(namedtuple('PrimitiveParts', ['type_name', 'type_params']))
     def __str__(self):
         return f"{self.type_name}:{self.type_params}"
 
-# Util functions to parse the Thrift column types (as strings) and return the expected hive type
-class TypeParser:
 
+# Util functions to parse the Thrift column types (as strings) and return the
+# expected hive type
+class TypeParser:
     def __init__(self, type_info_string: str):
         self.type_string = type_info_string
         self.type_tokens = self.tokenize(type_info_string)
         self.index = 0
-        
-    # we want the base name of the type, in general the format of a parameterized type is <base_name>(<type1>, <type2>, ...), so the base name is the string before the first '('
+
+    # we want the base name of the type, in general the format of a
+    # parameterized type is <base_name>(<type1>, <type2>, ...), so the base
+    # name is the string before the first '('
     @staticmethod
     def get_base_name(type_name: str) -> str:
-        index = type_name.find('(')
+        index = type_name.find("(")
         if index == -1:
             return type_name
         return type_name[:index]
-    
+
     # Is the type named using the allowed characters?
     @staticmethod
     def is_valid_type_char(c: str) -> bool:
-        return c.isalnum() or c in ('_', '.', ' ', '$')
-    
-    # The concept of the tokenizer is to split the type string into tokens. The tokens are either type names or type parameters. The type parameters are enclosed in parentheses.
+        return c.isalnum() or c in ("_", ".", " ", "$")
+
+    # The concept of the tokenizer is to split the type string into tokens. The
+    # tokens are either type names or type parameters. The type parameters are
+    # enclosed in parentheses.
     def tokenize(self, type_info_string: str) -> List[Token]:
         tokens = []
         begin = 0
         end = 1
         while end <= len(type_info_string):
-            if begin > 0 and type_info_string[begin - 1] == '(' and type_info_string[begin] == "'":
+            if (
+                begin > 0
+                and type_info_string[begin - 1] == "("
+                and type_info_string[begin] == "'"
+            ):
                 begin += 1
                 end += 1
                 while type_info_string[end] != "'":
                     end += 1
 
-            elif type_info_string[begin] == "'" and type_info_string[begin + 1] == ')':
+            elif type_info_string[begin] == "'" and type_info_string[begin + 1] == ")":
                 begin += 1
                 end += 1
 
@@ -231,43 +259,63 @@ class TypeParser:
                 or not self.is_valid_type_char(type_info_string[end - 1])
                 or not self.is_valid_type_char(type_info_string[end])
             ):
-                token = Token(begin, type_info_string[begin:end], self.is_valid_type_char(type_info_string[begin]))
+                token = Token(
+                    begin,
+                    type_info_string[begin:end],
+                    self.is_valid_type_char(type_info_string[begin]),
+                )
                 tokens.append(token)
                 begin = end
             end += 1
-        
+
         return tokens
-    
-    def peek(self) -> Token:
+
+    def peek(self) -> Optional[Token]:
         if self.index >= len(self.type_tokens):
             return None
         return self.type_tokens[self.index]
 
-    def expect(self, item: str, alternative: str = None) -> Token:
+    def expect(self, item: str, alternative: Optional[str] = None) -> Token:
         if self.index >= len(self.type_tokens):
             raise ValueError(f"Error: {item} expected at the end of 'typeInfoString'")
 
         token = self.type_tokens[self.index]
-        
+
         if item == "type":
-            if token.text not in [HTypeCategory.LIST.value,
-                                HTypeCategory.MAP.value,
-                                HTypeCategory.STRUCT.value,
-                                HTypeCategory.UNION.value] and (self.get_base_name(token.text) is None) and (token.text != alternative):
-                raise ValueError(f"Error: {item} expected at the position {token.position} of 'typeInfoString' but '{token.text}' is found.")
+            if (
+                token.text
+                not in [
+                    HTypeCategory.LIST.value,
+                    HTypeCategory.MAP.value,
+                    HTypeCategory.STRUCT.value,
+                    HTypeCategory.UNION.value,
+                ]
+                and (self.get_base_name(token.text) is None)
+                and (token.text != alternative)
+            ):
+                raise ValueError(
+                    f"Error: {item} expected at the position {token.position} "
+                    f"of 'typeInfoString' but '{token.text}' is found."
+                )
         elif item == "name":
             if not token.type and token.text != alternative:
-                raise ValueError(f"Error: {item} expected at the position {token.position} of 'typeInfoString' but '{token.text}' is found.")
+                raise ValueError(
+                    f"Error: {item} expected at the position {token.position} "
+                    f"of 'typeInfoString' but '{token.text}' is found."
+                )
         elif item != token.text and token.text != alternative:
-            raise ValueError(f"Error: {item} expected at the position {token.position} of 'typeInfoString' but '{token.text}' is found.")
+            raise ValueError(
+                f"Error: {item} expected at the position {token.position} of "
+                f"'typeInfoString' but '{token.text}' is found."
+            )
 
-        self.index += 1 # increment the index
+        self.index += 1  # increment the index
         return token
-    
+
     def parse_params(self) -> List[str]:
         params = []
 
-        token: Token = self.peek()
+        token: Optional[Token] = self.peek()
         if token is not None and token.text == "(":
             token = self.expect("(", None)
             token = self.peek()
@@ -277,30 +325,45 @@ class TypeParser:
                 token = self.expect(",", ")")
 
             if not params:
-                raise ValueError("type parameters expected for type string 'typeInfoString'")
+                raise ValueError(
+                    "type parameters expected for type string 'typeInfoString'"
+                )
 
         return params
-    
+
     def parse_type(self) -> HType:
-        token: Token = self.expect("type")
+        token = self.expect("type")
 
         # first we take care of primitive types.
         serde_val = get_serde_type_by_value(token.text)
-        if serde_val not in (HTypeCategory.LIST.value, HTypeCategory.MAP.value, HTypeCategory.STRUCT.value, HTypeCategory.UNION.value):
+        if serde_val and serde_val not in (
+            HTypeCategory.LIST.value,
+            HTypeCategory.MAP.value,
+            HTypeCategory.STRUCT.value,
+            HTypeCategory.UNION.value,
+        ):
             primitive_type = serde_to_primitive(serde_val)
-            if primitive_type is not PrimitiveCategory.UNKNOWN:
+            if primitive_type and primitive_type is not PrimitiveCategory.UNKNOWN:
                 params = self.parse_params()
-                if primitive_type in (PrimitiveCategory.CHAR, PrimitiveCategory.VARCHAR):
+                if primitive_type in (
+                    PrimitiveCategory.CHAR,
+                    PrimitiveCategory.VARCHAR,
+                ):
                     if len(params) == 0:
-                        raise ValueError("char/varchar type must have a length specified")
+                        raise ValueError(
+                            "char/varchar type must have a length specified"
+                        )
                     if len(params) == 1:
                         length = int(params[0])
                         if primitive_type is PrimitiveCategory.CHAR:
                             return HCharType(length)
                         elif primitive_type is PrimitiveCategory.VARCHAR:
                             return HVarcharType(length)
-                    else: 
-                        raise ValueError(f"Error: {token.text} type takes only one parameter, but instead {len(params)} parameters are found.")
+                    else:
+                        raise ValueError(
+                            f"Error: {token.text} type takes only one parameter, but instead "
+                            f"{len(params)} parameters are found."
+                        )
                 elif primitive_type is PrimitiveCategory.DECIMAL:
                     if len(params) == 0:
                         return HDecimalType(10, 0)
@@ -312,18 +375,20 @@ class TypeParser:
                         scale = int(params[1])
                         return HDecimalType(precision, scale)
                     else:
-                        raise ValueError(f"Error: {token.text} type takes only two parameters, but instead {len(params)} parameters are found.")
+                        raise ValueError(
+                            f"Error: {token.text} type takes only two parameters, but instead "
+                            f"{len(params)} parameters are found."
+                        )
                 else:
-                    
                     return HPrimitiveType(primitive_type)
-        
+
         # next we take care of complex types.
         if HTypeCategory.LIST.value == serde_val:
             self.expect("<")
             element_type = self.parse_type()
             self.expect(">")
             return HListType(element_type)
-        
+
         if HTypeCategory.MAP.value == serde_val:
             self.expect("<")
             key_type = self.parse_type()
@@ -331,25 +396,25 @@ class TypeParser:
             value_type = self.parse_type()
             self.expect(">")
             return HMapType(key_type, value_type)
-        
+
         if HTypeCategory.STRUCT.value == serde_val:
             self.expect("<")
             names = []
             types = []
-            token = self.peek()
+            token: Optional[Token] = self.peek()
             while token is not None and token.text != ">":
                 field_name = self.expect("name")
                 self.expect(":")
                 field_type = self.parse_type()
                 names.append(field_name.text)
                 types.append(field_type)
-                token = self.peek()
-                if token.text == ",":
+                token: Optional[Token] = self.peek()
+                if token is not None and token.text == ",":
                     self.expect(",")
                     token = self.peek()
             self.expect(">")
             return HStructType(names, types)
-        
+
         if HTypeCategory.UNION.value == serde_val:
             self.expect("<")
             types = []
@@ -358,15 +423,12 @@ class TypeParser:
                 field_type = self.parse_type()
                 types.append(field_type)
                 token = self.peek()
-                if token.text == ",":
+                if token is not None and token.text == ",":
                     self.expect(",")
                     token = self.peek()
             self.expect(">")
             return HUnionType(types)
-        
-        # if we reach this point and we haven't figure out what to do, then we better raise an error.
-        raise ValueError(f"Error: {token.text} is not a valid type.")
-    
 
-parser = TypeParser("struct<name:string,age:int>")
-ptype:HStructType = parser.parse_type()
+        # if we reach this point and we haven't figure out what to do, then we
+        # better raise an error.
+        raise ValueError(f"Error: {token.text} is not a valid type.")
--- pymetastore/metastore.py
+++ pymetastore/metastore.py
@@ -1,88 +1,105 @@
 from enum import Enum
-from typing import Dict, List
-from htypes import HType
-from thrift.transport import TTransport, TSocket
-from thrift.protocol.TBinaryProtocol import TBinaryProtocol
-from hms.ttypes import *
+from typing import Dict, List, Optional
+
 from hms import ThriftHiveMetastore as hms
+from hms.ttypes import *
+from htypes import HType
+from thrift.protocol.TBinaryProtocol import TBinaryProtocol
+from thrift.transport import TSocket, TTransport
+
 
 class HPrincipalType(Enum):
     ROLE = "ROLE"
     USER = "USER"
 
+
 class HDatabase:
-    def __init__(self, name: str, 
-                 location: str =None, 
-                 ownerName:str =None, 
-                 ownerType: HPrincipalType =None, 
-                 comment=None, 
-                 parameters: Dict[str, str]=None):
-        
+    def __init__(
+        self,
+        name: str,
+        location: Optional[str] = None,
+        owner_name: Optional[str] = None,
+        owner_type: Optional[HPrincipalType] = None,
+        comment: Optional[str] = None,
+        parameters: Optional[Dict[str, str]] = None,
+    ):
         self.name = name
         self.location = location
-        self.ownerName = ownerName
-        self.ownerType = ownerType
+        self.owner_name = owner_name
+        self.owner_type = owner_type
         self.comment = comment
         self.parameters = parameters
 
+
 class HColumn:
-    def __init__(self, name: str, type: HType, comment: str =None):
+    def __init__(self, name: str, type: HType, comment: Optional[str] = None):
         self.name = name
         self.type = type
         self.comment = comment
 
-#TODO: Implement
+
+# TODO: Implement
 class HiveBucketProperty:
     def __init__(self):
         # TODO!!
         pass
 
+
 class StorageFormat:
-    def __init__(self, serde: str, inputFormat: str, outputFormat: str) -> None:
+    def __init__(self, serde: str, input_format: str, output_format: str) -> None:
         self.serde = serde
-        self.inputFormat = inputFormat
-        self.outputFormat = outputFormat
+        self.input_format = input_format
+        self.output_format = output_format
+
 
 class HStorage:
-    def __init__(self, storageFormat: StorageFormat,
-                 skewed: bool = False, 
-                 location: str =None, 
-                 bucketProperty: HiveBucketProperty =None, 
-                 serdeParameters: Dict[str, str] =None):
-        
-        if storageFormat is None:
+    def __init__(
+        self,
+        storage_format: StorageFormat,
+        skewed: bool = False,
+        location: Optional[str] = None,
+        bucket_property: Optional[HiveBucketProperty] = None,
+        serde_parameters: Optional[Dict[str, str]] = None,
+    ):
+        if storage_format is None:
             raise ValueError("storageFormat cannot be None")
-        self.storageFormat = storageFormat
+        self.storage_format = storage_format
         self.skewed = skewed
         self.location = location
-        self.bucketProperty = bucketProperty
-        self.serdeParameters = serdeParameters
+        self.bucket_property = bucket_property
+        self.serde_parameters = serde_parameters
+
 
 class HTable:
-    def __init__(self, databaseName: str, name: str, 
-                 tableType: str, columns: List[HColumn], 
-                 partitionColumns: List[HColumn],
-                 storage: HStorage, 
-                 parameters: Dict[str, str], 
-                 viewOriginalText: str = None, 
-                 viewExpandedText: str = None, 
-                 writeId: int = None, 
-                 owner: str = None):
-        
-        self.databaseName = databaseName
+    def __init__(
+        self,
+        database_name: str,
+        name: str,
+        table_type: str,
+        columns: List[HColumn],
+        partition_columns: List[HColumn],
+        storage: HStorage,
+        parameters: Dict[str, str],
+        view_original_text: Optional[str] = None,
+        view_expanded_text: Optional[str] = None,
+        write_id: Optional[int] = None,
+        owner: Optional[str] = None,
+    ):
+        self.database_name = database_name
         self.name = name
         self.storage = storage
-        self.tableType = tableType
+        self.table_type = table_type
         self.columns = columns
-        self.partitionColumns = partitionColumns
+        self.partition_columns = partition_columns
         self.parameters = parameters
-        self.viewOriginalText = viewOriginalText
-        self.viewExpandedText = viewExpandedText
-        self.writeId = writeId
+        self.view_original_text = view_original_text
+        self.view_expanded_text = view_expanded_text
+        self.write_id = write_id
         self.owner = owner
 
+
 class HMS:
-    def __init__(self, host='localhost', port=9090):
+    def __init__(self, host="localhost", port=9090):
         self.host = host
         self.port = port
         self.socket = TSocket.TSocket(self.host, self.port)
@@ -102,30 +119,41 @@ class HMS:
         for database in databases:
             db_names.append(database.name)
         return db_names
-            
+
     def get_database(self, name: str) -> HDatabase:
         db: Database = self.client.get_database(name)
-        
-        if db.ownerType is PrincipalType.USER:
-            ownerType = HPrincipalType.USER
-        elif db.ownerType is PrincipalType.ROLE:
-            ownerType = HPrincipalType.ROLE
-        else:
-            ownerType = None
 
-        return HDatabase(db.name, db.locationUri, db.ownerName, ownerType, db.description, db.parameters)
-    
+        if db.ownerType is PrincipalType.USER:
+            owner_type = HPrincipalType.USER
+        elif db.ownerType is PrincipalType.ROLE:
+            owner_type = HPrincipalType.ROLE
+        else:
+            owner_type = None
+
+        return HDatabase(
+            db.name,  # pyright: ignore[reportGeneralTypeIssues]
+            db.locationUri,
+            db.ownerName,
+            owner_type,
+            db.description,
+            db.parameters,
+        )
+
     def list_tables(self, databaseName: str) -> List[str]:
         tables = self.client.get_all_tables(databaseName)
         table_names = []
         for table in tables:
             table_names.append(table.tableName)
         return table_names
-    
+
     def list_columns(self, databaseName: str, tableName: str) -> List[str]:
-        columns = self.client.get_table(databaseName, tableName).sd.cols
+        # TODO: Rather than ignore these pyright errors, do appropriate None handling
+        columns = self.client.get_table(
+            databaseName,
+            tableName,
+        ).sd.cols  # pyright: ignore[reportOptionalMemberAccess]
         self.client.get_schema(databaseName, tableName)
         column_names = []
-        for column in columns:
+        for column in columns:  # pyright: ignore[reportOptionalIterable]
             column_names.append(column.name)
         return column_names
