182f2a05f37797822be36a5153193e4b6b6b3faf
Chris Riccomini <criccomini@apache.org>
2023-06-21 12:33:08 -0700

    Add integration test for HMS
    Spin up a Hive Metastore service in a Github workflow and run integration tests
    against it.
    
    The integration test right now is just a little smoke test to prove the
    integration tests work. We can add real tests in a subsequent commit.
    
    I ended up having to roll my own Hive Metastore Docker image, which can be found
    here:
    
    https://github.com/criccomini/hive-metastore-standalone
    
    The integration test connects to this HMS service and uses it for tests.

---
+++ tests/integration/test_metastore.py
@@ -0,0 +1,42 @@
+import os
+
+import pytest
+from thrift.protocol import TBinaryProtocol
+from thrift.transport import TSocket, TTransport
+
+from pymetastore.hms import ttypes
+from pymetastore.hms.ThriftHiveMetastore import Client
+from pymetastore.metastore import HMS
+
+
+@pytest.fixture(scope="module")
+def hive_client():
+    host = os.environ.get("HMS_HOST", "localhost")
+    port = int(os.environ.get("HMS_PORT", 9083))
+    transport = TSocket.TSocket(host, port)
+    transport = TTransport.TBufferedTransport(transport)
+    protocol = TBinaryProtocol.TBinaryProtocol(transport)
+    client = Client(protocol)
+    transport.open()
+
+    yield client
+
+    transport.close()
+
+
+@pytest.fixture(scope="module", autouse=True)
+def setup_data(hive_client):
+    db = ttypes.Database(
+        name="test_db",
+        description="This is a test database",
+        locationUri="/tmp/test_db.db",
+        parameters={},
+    )
+
+    if "test_db" in hive_client.get_all_databases():
+        hive_client.drop_database("test_db", True, True)
+    hive_client.create_database(db)
+
+
+def test_database_exists(hive_client):
+    assert "test_db" in HMS(hive_client).list_databases()
